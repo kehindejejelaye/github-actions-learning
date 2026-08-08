b76b9b3d519fd1e077dbb04286b83a4f9601cefa
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2026-08-08 10:21:14 +0100

    feat: add multiply function

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ function add(a, b) {
   return a + b;
 }
 
+function multiply(a, b) {
+  return a * b;
+}
+
 console.log(add(2, 3));
+console.log(multiply(2, 3));
--- tests/add.test.js
+++ tests/add.test.js
@@ -5,6 +5,14 @@ function add(a, b) {
   return a + b;
 }
 
+function multiply(a, b) {
+  return a * b;
+}
+
 test('adds two numbers', () => {
   assert.strictEqual(add(2, 3), 5);
+});
+
+test('multiplies two numbers', () => {
+  assert.strictEqual(multiply(2, 3), 6);
 });
